9b9f59318ba357a64b64df75d84385c4c2437893
yuzhang <yuzhang2002@gmail.com>
2023-01-20 15:38:58 +1100

    [world] added two walls

--- box2d-demo.js
+++ box2d-demo.js
@@ -14,6 +14,7 @@ const timeStep = 1 / 60;
 const velocityIterations = 8;
 const positionIterations = 3;
 let context;
+const radius = 5;
 
 function init() {
     let gravity = new b2Vec2(0, 9.8);
@@ -21,9 +22,11 @@ function init() {
 
     world = new b2World(gravity, allowSleep);
     createFloor();
+    createWall(5, 0);
+    createWall(645, 0);
     createRectangularBody();
-    for (let index = 0; index < 50; index++) {
-        createCircularBody(index+40, index+10, index+0.01);
+    for (let index = 0; index < 300; index++) {
+        createCircularBody(index+10, index+10, radius);
     }
     setupDebugDraw();
     animate();
@@ -34,6 +37,26 @@ function animate() {
     world.ClearForces();
     world.DrawDebugData();
     setTimeout(animate, timeStep);
+}
+
+function createWall(positionX, positionY) {
+    let bodyDef = new b2BodyDef;
+
+    bodyDef.type = b2Body.b2_staticBody;
+    bodyDef.position.x = positionX / scale;
+    bodyDef.position.y = positionY / scale;
+
+    let fixtureDef = new b2FixtureDef;
+
+    fixtureDef.density = 1.0;
+    fixtureDef.friction = 0.5;
+    fixtureDef.restitution = 0.2;
+
+    fixtureDef.shape = new b2PolygonShape;
+    fixtureDef.shape.SetAsBox(10 / scale, 480 / scale);
+
+    let body = world.CreateBody(bodyDef);
+    let fixture = body.CreateFixture(fixtureDef);
 }
 
 function createFloor() {
